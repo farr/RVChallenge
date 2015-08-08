9f7cccb5bc3a73940fee90406eaed00c26801183
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-08-07 22:29:11 -1000

    Fixed AR(1) model code; tested with Ensemble.

--- julia/AR1.jl
+++ julia/AR1.jl
@@ -14,58 +14,44 @@ immutable AR1Model
     nu::Float64
 end
 
-immutable AR1State
-    mu::Float64
-    var::Float64
-end
-
-function observe(model::AR1Model, state::AR1State, x::Float64, dx::Float64)
-    dx2 = dx*dx*model.nu*model.nu
-
-    mu_new = (state.mu*dx2 + x*state.var)/(dx2 + state.var)
-    var_new = 1.0/(1.0/dx2 + 1.0/state.var)
-
-    AR1State(mu_new, var_new)
-end
-
-function advance(model::AR1Model, state::AR1State, dt::Float64)
-    y = state.mu - model.mu
-
-    alpha = exp(-dt/model.tau)
-    
-    y_new = alpha*y
-
-    mu_new = y_new + model.mu
-
-    a2 = alpha*alpha
-    
-    var_new = a2*state.var + (1.0 - a2)*model.sigma*model.sigma
-
-    AR1State(mu_new, var_new)
-end
-
 """ Returns the uncorrelated residuals and associated uncertainties
 for the AR(1) model applied to the given data. """
 function residuals(model::AR1Model, ts::Array{Float64, 1}, ys::Array{Float64, 1}, dys::Array{Float64, 1})
     n = size(ts, 1)
 
-    state = AR1State(model.mu, model.sigma*model.sigma)
-    resids = zeros(n)
-    sresids = zeros(n)
-    t = ts[1]
+    s2 = model.sigma*model.sigma
+    nu2 = model.nu*model.nu
 
+    rs = zeros(n)
+    ss = zeros(n)
+
+    t = ts[1]
+    x = 0.0
+    dx2 = s2
+    k = 1.0
+    
     for i in 1:n
+        # Advance the filter
         dt = ts[i] - t
         t = ts[i]
-        state = advance(model, state, dt)
-        
-        resids[i] = ys[i] - state.mu
-        sresids[i] = sqrt(state.var + dys[i]*dys[i])
+        lam = exp(-dt/model.tau)
+        x = lam*x
+        dx2 = lam*lam*(dx2 - s2) + s2
 
-        state = observe(model, state, ys[i], dys[i])
+        # Predict the observation
+        rs[i] = ys[i] - x - model.mu
+        ss2 = dx2 + nu2*dys[i]*dys[i]
+        ss[i] = sqrt(ss2)
+
+        # Update gain
+        k = dx2 / ss2
+
+        # Update state
+        x += rs[i]*k
+        dx2 -= ss2*k*k
     end
 
-    resids, sresids
+    rs, ss
 end
 
 """ Returns a sample from the given AR(1) model at times `ts` with
@@ -75,7 +61,7 @@ function simulate(model::AR1Model, ts::Array{Float64, 1}, dxs::Array{Float64, 1}
     xsout = zeros(ts)
 
     xs[1] = model.sigma*randn()
-    xsout[1] = xs[1] + model.nu*dxs[1]*randn()
+    xsout[1] = xs[1] + model.nu*dxs[1]*randn() + model.mu
     
     for i in 2:size(xs,1)
         dt = ts[i] - ts[i-1]
