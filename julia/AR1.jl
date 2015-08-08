dddff13e6d7ba995da41741158e2830fcef3cf16
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-08-07 19:14:46 -1000

    Corrected AR(1) bugs, and added simulate method.

--- julia/AR1.jl
+++ julia/AR1.jl
@@ -1,11 +1,17 @@
 module AR1
 
-export AR1Model, AR1State, residuals
+export AR1Model, residuals, simulate
 
+@doc """
+Construct an AR(1) model with mean `mu`, standard deviation (at zero
+lag) `sigma`, correlation time `tau`, and measurement uncertainty scale
+`nu`.
+""" ->
 immutable AR1Model
     mu::Float64
     sigma::Float64
     tau::Float64
+    nu::Float64
 end
 
 immutable AR1State
@@ -13,8 +19,8 @@ immutable AR1State
     var::Float64
 end
 
-function observe(state::AR1State, x::Float64, dx::Float64)
-    dx2 = dx*dx
+function observe(model::AR1Model, state::AR1State, x::Float64, dx::Float64)
+    dx2 = dx*dx*model.nu*model.nu
 
     mu_new = (state.mu*dx2 + x*state.var)/(dx2 + state.var)
     var_new = 1.0/(1.0/dx2 + 1.0/state.var)
@@ -38,6 +44,8 @@ function advance(model::AR1Model, state::AR1State, dt::Float64)
     AR1State(mu_new, var_new)
 end
 
+""" Returns the uncorrelated residuals and associated uncertainties
+for the AR(1) model applied to the given data. """
 function residuals(model::AR1Model, ts::Array{Float64, 1}, ys::Array{Float64, 1}, dys::Array{Float64, 1})
     n = size(ts, 1)
 
@@ -54,10 +62,32 @@ function residuals(model::AR1Model, ts::Array{Float64, 1}, ys::Array{Float64, 1}
         resids[i] = ys[i] - state.mu
         sresids[i] = sqrt(state.var + dys[i]*dys[i])
 
-        state = observe(state, ys[i], dys[i])
+        state = observe(model, state, ys[i], dys[i])
     end
 
     resids, sresids
+end
+
+""" Returns a sample from the given AR(1) model at times `ts` with
+observational uncertainties `dxs` """
+function simulate(model::AR1Model, ts::Array{Float64, 1}, dxs::Array{Float64, 1})
+    xs = zeros(ts)
+    xsout = zeros(ts)
+
+    xs[1] = model.sigma*randn()
+    xsout[1] = xs[1] + model.nu*dxs[1]*randn()
+    
+    for i in 2:size(xs,1)
+        dt = ts[i] - ts[i-1]
+        alpha = exp(-dt/model.tau)
+
+        beta = model.sigma*sqrt(1.0 - alpha*alpha)
+        
+        xs[i] = alpha*xs[i-1] + beta*randn()
+        xsout[i] = xs[i] + model.nu*dxs[i]*randn() + model.mu
+    end
+
+    xsout
 end
 
 end
